191ef3b0974b685010aa6635ca6267c95681301b
Alexander Shakola <84412811+umbraz1@users.noreply.github.com>
2022-09-06 16:09:55 +0300

    Add files via upload

---
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -0,0 +1,32 @@
+package com.epam.rd.autotasks.matrices;
+import java.util.Arrays;
+
+public class MultiplyMatrix {
+    public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        int[][] resultMatrix = new int[matrix1.length][matrix2[0].length];
+
+        for (int i = 0; i < resultMatrix.length; i++) {
+            for (int j = 0; j < resultMatrix[i].length; j++) {
+                for (int k = 0; k < matrix1[0].length; k++) {
+                    resultMatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        return resultMatrix;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] a = {
+                {0, 12345},
+                {4509, 0},
+                {3, 567} };
+
+        int[][] b = {
+                {653, 0, 25353},
+                {0, 61, 6} };
+
+        int[][] result = multiply(a, b);
+        System.out.println(Arrays.deepToString(result).replace("],", "]\n"));
+    }
+}
